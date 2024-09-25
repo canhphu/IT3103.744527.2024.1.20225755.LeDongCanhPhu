e6fdd94979851b34bc09614097e3ff13d55b826a
canhphu <canhphu131204@gmail.com>
2024-09-25 14:47:13 +0700

    Update HelloNameDialog.java

--- lab1/HelloNameDialog.java
+++ lab1/HelloNameDialog.java
@@ -4,8 +4,8 @@ import javax.swing.JOptionPane;
 public class HelloNameDialog {
     public static void main(String[] args) {
         String result;
-        result = JOptionPane.showInputDialog("Please enter your name:");
-        JOptionPane.showMessageDialog(null, "Hi " + result + "!");
+        result = JOptionPane.showInputDialog("Le Dong Canh Phu - 20225755 - Please enter your name:");
+        JOptionPane.showMessageDialog(null, "Toi ten la Le Dong Canh Phu -20225755 - Hi " + result + "!");
         System.exit(0);
     }
 }
