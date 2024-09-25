f651b51df06a2f7c9dae4dca5f135368c877dab5
canhphu <canhphu131204@gmail.com>
2024-09-25 14:43:28 +0700

    Update HelloWorld.java

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -3,6 +3,7 @@ package lab1;
 //Text-printing program.
 public class HelloWorld {
     public static void main(String args[]) {
+        System.out.println("Le Dong Canh Phu - 20225755");
         System.out.println("Xin chao \n cac ban");
         System.out.println("Hello \t World");
     } // end of method main
