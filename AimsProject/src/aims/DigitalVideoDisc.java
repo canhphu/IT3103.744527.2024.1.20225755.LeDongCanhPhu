6d97525331cfda48a3226ec59fd0324ac41099d4
canhphu <canhphu131204@gmail.com>
2024-10-28 23:48:03 +0700

    update DigitalVideoDisc.java

--- AimsProject/src/aims/DigitalVideoDisc.java
+++ AimsProject/src/aims/DigitalVideoDisc.java
@@ -21,8 +21,30 @@ public class DigitalVideoDisc {
     public float getCost() {
         return cost;
     }
+    
     public DigitalVideoDisc(String title) {
-        super();
         this.title = title;
     }
+    
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+   
 }
