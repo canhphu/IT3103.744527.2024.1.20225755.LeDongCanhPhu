20b3c47e20cc77e3a2b72cf2293975e11154f004
canhphu <canhphu131204@gmail.com>
2024-10-28 21:59:55 +0700

    Delete folder lab1

--- lab1/FirstDialog.java
+++
@@ -1,9 +0,0 @@
-//Example 2: FirstDialog.java
-package lab1;
-import javax.swing.JOptionPane;
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Le Dong Canh Phu -20225755-Hello world! How are you ?");
-        System.exit(0);
-    }
-}
