364b5488f0ccf6a16fc7fe340fbbbc2e8f22d61c
canhphu <canhphu131204@gmail.com>
2024-10-28 23:13:07 +0700

    create Cart.java

---
+++ AimsProject/src/aims/Cart.java
@@ -0,0 +1,26 @@
+package aims;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
+    public boolean addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if(qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added.");
+            return true;
+        }
+        else {
+            System.out.println("The cart is almost full.");
+            return false;
+        }
+    }
+    public int totalCost() {
+        int total = 0;
+        for(int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
