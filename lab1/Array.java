a73082fd94a6ec679ea9d8bbd1f3911c25a21dc8
canhphu <canhphu131204@gmail.com>
2024-10-30 23:17:08 +0700

    Restore lab1

---
+++ lab1/Array.java
@@ -0,0 +1,37 @@
+package lab1;
+import java.util.Scanner;
+public class Array {
+    public static void main (String[] args) {
+        int[] arr = {};
+        int sum = 0;
+        int avg = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Le Dong Canh Phu - 20225755 - Enter the number of elements: "); //Enter the number of elements
+        int n = input.nextInt();
+        arr = new int[n]; // Create an array with n elements
+        for(int i = 0; i < n; i++) {
+            System.out.print("Enter the element " + (i+1) + ": "); //Enter the element
+            arr[i] = input.nextInt();
+            sum += arr[i]; // Calculate the sum of the elements
+        }
+        // Sort the array in ascending order
+        for(int i = 0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if(arr[i]>arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.print("Le Dong Canh Phu - 20225755 - The array in ascending order: ");
+        for(int i = 0;i<n;i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        avg = sum/n; // Calculate the average of the elements
+        System.out.println("Le Dong Canh Phu - 20225755 - The sum of the elements is: " + sum);
+        System.out.println("Le Dong Canh Phu - 20225755 - The average of the elements is: " + avg);
+        input.close();
+    }
+}
---
+++ lab1/Matrices.java
@@ -0,0 +1,35 @@
+package lab1;
+import java.util.Scanner;
+
+public class Matrices {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Le Dong Canh Phu - 20225755");
+        System.out.print("Enter the number of rows: "); //Enter the number of rows
+        int m = input.nextInt();
+        System.out.print("Enter the number of columns: "); //Enter the number of columns
+        int n = input.nextInt();
+        int[][] matrix1 = new int[m][n]; // Create a matrix with m rows and n columns
+        int[][] matrix2 = new int[m][n]; 
+            System.out.println("Matrix 1: ");
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    matrix1[i][j] = input.nextInt();
+                }
+            }
+            System.out.println("Matrix 2: ");
+            for(int i=0;i<m;i++) {
+                for(int j=0;j<n;j++) {
+                    matrix2[i][j] = input.nextInt();
+                }
+            }
+        System.out.println("Sum of two matrices: ");
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                System.out.print(matrix1[i][j] + matrix2[i][j] + " "); // Print the sum of two matrices
+            }
+            System.out.println();
+        }       
+        input.close();        
+    }
+}
