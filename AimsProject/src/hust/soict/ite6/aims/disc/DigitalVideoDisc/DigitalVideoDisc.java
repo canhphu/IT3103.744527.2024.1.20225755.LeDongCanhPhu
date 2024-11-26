d28c8135226780006381ee10955db0df39533c6e
canhphu <canhphu131204@gmail.com>
2024-11-26 22:07:13 +0700

    Update new swap method

--- AimsProject/src/hust/soict/ite6/aims/disc/DigitalVideoDisc/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/ite6/aims/disc/DigitalVideoDisc/DigitalVideoDisc.java
@@ -59,6 +59,18 @@ public class DigitalVideoDisc {
         this.title = title;
     }
     
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
     public static int getNbDigitalVideoDiscs() { 
         return nbDigitalVideoDiscs; // Get the number of DigitalVideoDisc objects created
     }
--- AimsProject/src/hust/soict/ite6/test/disc/TestPassingParameter/TestPassingParameter.java
+++ AimsProject/src/hust/soict/ite6/test/disc/TestPassingParameter/TestPassingParameter.java
@@ -12,8 +12,8 @@ public class TestPassingParameter {
         System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
         System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
-        changeTitle(jungleDVD, cinderellaDVD.getTitle());
-        System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
+        // changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        // System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
     }
 
     public static void swap(Object o1, Object o2) {
@@ -27,4 +27,25 @@ public class TestPassingParameter {
         dvd.setTitle(title);
         dvd = new DigitalVideoDisc(oldTitle);
     }
+
+    public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        if(dvd1 == null || dvd2 == null) {
+            System.out.println("One of the discs is null");
+            return;
+        }
+        DigitalVideoDisc temp = new DigitalVideoDisc(dvd1.getTitle(), dvd1.getCategory(), 
+        dvd1.getDirector(), dvd1.getLength(), dvd1.getCost()); // Create a temporary disc to store the information of dvd1
+        dvd1.setTitle(dvd2.getTitle());
+        dvd1.setCategory(dvd2.getCategory());
+        dvd1.setDirector(dvd2.getDirector());
+        dvd1.setLength(dvd2.getLength());
+        dvd1.setCost(dvd2.getCost());
+        dvd2.setTitle(temp.getTitle());
+        dvd2.setCategory(temp.getCategory());
+        dvd2.setDirector(temp.getDirector());
+        dvd2.setLength(temp.getLength());
+        dvd2.setCost(temp.getCost());
+    }
+
+    
 }
