b95a30770172f447cecfa5e865e37eca9cb0d566
canhphu <canhphu131204@gmail.com>
2024-12-01 00:44:24 +0700

    compact disc

--- AimsProject/src/hust/soict/ite6/aims/media/CompactDisc.java
+++ AimsProject/src/hust/soict/ite6/aims/media/CompactDisc.java
@@ -1,5 +1,52 @@
 package hust.soict.ite6.aims.media;
 
-public class CompactDisc {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CompactDisc extends Disc {
+    private String artist;
+    private List<Track> tracks = new ArrayList<Track>();
+    public String getArtist() {
+        return artist;
+    }
+    public CompactDisc(String title, String artist) {
+        super(title);
+        this.artist = artist;
+    }
+    public CompactDisc(String title, String category, float cost, String artist) {
+        super(title, category, cost);
+        this.artist = artist;
+    }
+    public CompactDisc(String title, String category, String director, float cost, String artist) {
+        super(title, category, director, cost);
+        this.artist = artist;
+    }
+    public CompactDisc(String title, String category, float cost, String director, int length, String artist) {
+        super(title, category, cost, director, length);
+        this.artist = artist;
+    }
+    public void addTrack(Track track) {
+        if(tracks.indexOf(track)==-1) {
+            System.out.println("Track is already in list");
+            return;
+        }
+        tracks.add(track);
+        System.out.println("Add track successfully");
+    }
+    public void removeTrack(Track track) {
+        if(tracks.indexOf(track) == -1) {
+            System.out.println("Track is not in list");
+            return;
+        }
+        tracks.remove(tracks.indexOf(track));
+        System.out.println("Remove track successfully");
+    }
+    public int getLength() {
+        int length = 0;
+        for(Track track : tracks) {
+            length += track.getLength();
+        }
+        setLength(length);
+        return length;
+    }
 }
