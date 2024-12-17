ef53fe8a3c77f87bfbaec4660ffe6136ecbac205
canhphu <canhphu131204@gmail.com>
2024-12-18 01:48:56 +0700

    modify track again

--- AimsProject/src/hust/soict/ite6/aims/media/Track.java
+++ AimsProject/src/hust/soict/ite6/aims/media/Track.java
@@ -1,32 +1,56 @@
 package hust.soict.ite6.aims.media;
 
 import hust.soict.ite6.aims.exception.PlayerException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Track implements Playable {
     private String title;
     private int length;
-    public String getTitle() {
-        return title;
-    }
-    public int getLength() {
-        return length;
-    }
+    private List<Track> tracks = new ArrayList<>();
+
     public Track(String title, int length) {
         this.title = title;
         this.length = length;
     }
+
     public Track(String title) {
         this.title = title;
     }
+
     public Track(int length) {
         this.length = length;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
     public void play() throws PlayerException {
-        if(this.getLength() > 0) {
-            System.out.println("Playing track: " + this.getTitle());
-            System.out.println("Track length: " + this.getLength());
-        } else {
-            throw new PlayerException("ERROR: Track length is non-positive");
+        try {
+            if (this.getLength() > 0) {
+                System.out.println("Playing track: " + this.getTitle());
+                System.out.println("Track length: " + this.getLength());
+                java.util.Iterator<Track> iter = tracks.iterator();
+                Track nextTrack;
+                while (iter.hasNext()) {
+                    nextTrack = iter.next();
+                    try {
+                        nextTrack.play();
+                    } catch (PlayerException e) {
+                        throw e;
+                    }
+                }
+            } else {
+                throw new PlayerException("ERROR: Track length is non-positive");
+            }
+        } catch (PlayerException e) {
+            System.err.println(e.getMessage());
+            throw e;
         }
     }
 
